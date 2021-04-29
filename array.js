9b1ba9acd73c4a0642d127a504813e5fb0ea9350
hansuja-yalavarthi <78600451+hansuja-yalavarthi@users.noreply.github.com>
2021-04-29 12:58:10 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,26 @@
+var string = "This is String";
+console.log(string);
+var num = 100;
+console.log(num);
+var bool = true;
+console.log(bool);
+var object;
+console.log(object);
+var object = null;
+console.log(object);
+var arr1 = [1, 2, 3];
+console.log(arr1);
+var arr2 = [1, "komal", null, 5];
+console.log(arr2);
+var arr3 = [
+    [1, 2, 3],
+    [4, 5, 6, 0, 0],
+    [7, 8, 9]
+];
+console.log(arr3[1][3]);
+var arr4 = ["Hansuja", "Komal", "Hitesh"];
+console.log(arr4);
+arr4.push("Sonal");
+console.log(arr4);
+arr4.pop();
+console.log(arr4);
